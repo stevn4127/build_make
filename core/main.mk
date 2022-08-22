GrapheneOS/platform_build

ignore tethering provisioning requirement

core/main.mk
@@ -267,6 +267,7 @@ ifneq (,$(user_variant))
   ADDITIONAL_DEFAULT_PROPERTIES += security.perf_harden=1
   ADDITIONAL_DEFAULT_PROPERTIES += ro.control_privapp_permissions=enforce
   ADDITIONAL_DEFAULT_PROPERTIES += persist.security.deny_new_usb=dynamic
+  ADDITIONAL_DEFAULT_PROPERTIES += net.tethering.noprovisioning=true
 
   ifeq ($(user_variant),user)
     ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1

stevn4127/build_make

Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>
Change-Id: I5db4f9908b352eb143124e0dc59dfa849d52757f

core/main.mk
@@ -368,6 +368,7 @@ ifneq (,$(user_variant))
   ADDITIONAL_SYSTEM_PROPERTIES += ro.secure=1
   ADDITIONAL_SYSTEM_PROPERTIES += security.perf_harden=1
   ADDITIONAL_SYSTEM_PROPERTIES += ro.control_privapp_permissions=enforce
+  ADDITIONAL_SYSTEM_PROPERTIES += net.tethering.noprovisioning=true
 
   ifeq ($(user_variant),user)
     ADDITIONAL_SYSTEM_PROPERTIES += ro.adb.secure=1